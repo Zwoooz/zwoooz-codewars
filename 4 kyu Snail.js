a0e0b8ba6c229ecf8f35ef68158705a473411e53
Benjamin S. Arvidsson <benjamin@Benjamins-MacBook-Air.local>
2022-11-09 00:46:21 +0100

    first commit

---
+++ 4 kyu Snail.js
@@ -0,0 +1,25 @@
+snail = function(array) {
+    let path = [];
+    while(array.length) {
+        array[0].forEach(element => {
+            path.push(element);
+        });
+        array.shift();
+
+        if(!array.length) return path;
+
+        for(let i = 0; i < array.length; i++) {
+            path.push((parseInt(array[i].splice(array[i].length-1,1).join(''))));
+        };
+
+        array[array.length-1].reverse().forEach(element => {
+            path.push(element);
+        });
+        array.pop();
+
+        for(let j = array.length-1; j >= 0; j--) {
+            path.push(parseInt(array[j].splice(0, 1).join('')));
+        };
+    };
+  return path;
+};
